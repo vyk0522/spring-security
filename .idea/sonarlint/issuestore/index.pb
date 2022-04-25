3772fa9ea7194db4a90050bf4ec1d2aab03f935c
Vivek Shankare Gowda <vgowda@onetrust.com>
2022-04-25 16:48:08 +0530

    Initial Check in after Resource Server and Client APP changes

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,15 @@
+
+T
+$web-application-client2/build.gradle,f\0\f00d6241d26bedf4d12e8f2864f35a5868505ac3
+W
+'web-application-client2/settings.gradle,1\4\14b8f1a6210b08bc347dc5100b595bcd25cbfa7c
+{
+Kresource-server/src/main/java/com/onejava/controller/RequestController.java,6\2\62c10e19986a603516146d225023e818fce9eb29
+x
+Hresource-server/src/main/java/com/onejava/ResourceServerApplication.java,e\8\e867616b5ffae4286c32b34d3d9b92310113f6a8
+z
+Jresource-server/src/main/java/com/onejava/config/ResourceServerConfig.java,d\7\d7c8c57cb99668912fa52a5a087b5507eea57f4d
+…
+Uweb-application-client/src/main/java/com/onejava/config/OAuthClientConfiguration.java,2\0\2010cbdf46d765950ecc713e67b9e7d12dcafa39
+…
+Uweb-application-client/src/main/java/com/onejava/WebApplicationClientApplication.java,b\e\be503141fc3d8d8b3dc18c9378a74daeaa77dabf
---
+++ webflux-client-2/src/main/java/com/onejava/config/OAuthClientConfiguration.java
@@ -0,0 +1,49 @@
+package com.onejava.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.client.AuthorizedClientServiceReactiveOAuth2AuthorizedClientManager;
+import org.springframework.security.oauth2.client.InMemoryReactiveOAuth2AuthorizedClientService;
+import org.springframework.security.oauth2.client.registration.ClientRegistration;
+import org.springframework.security.oauth2.client.registration.InMemoryReactiveClientRegistrationRepository;
+import org.springframework.security.oauth2.client.registration.ReactiveClientRegistrationRepository;
+import org.springframework.security.oauth2.client.web.reactive.function.client.ServerOAuth2AuthorizedClientExchangeFilterFunction;
+import org.springframework.security.oauth2.core.AuthorizationGrantType;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@Configuration
+public class OAuthClientConfiguration {
+
+    // Create client registration repository
+    @Bean
+    ReactiveClientRegistrationRepository oktaClientRegistration(
+            @Value("${spring.security.oauth2.client.provider.okta.token-uri}") String token_uri,
+            @Value("${spring.security.oauth2.client.registration.okta.client-id}") String client_id,
+            @Value("${spring.security.oauth2.client.registration.okta.client-secret}") String client_secret,
+            @Value("${spring.security.oauth2.client.registration.okta.scope}") String scope,
+            @Value("${spring.security.oauth2.client.registration.okta.authorization-grant-type}") String authorizationGrantType
+    ) {
+        ClientRegistration clientRegistration = ClientRegistration
+                .withRegistrationId("client2") // Can be anything
+                .tokenUri(token_uri)
+                .clientId(client_id)
+                .clientSecret(client_secret)
+                .scope(scope)
+                .authorizationGrantType(new AuthorizationGrantType(authorizationGrantType))
+                .build();
+        return new InMemoryReactiveClientRegistrationRepository(clientRegistration);
+    }
+
+    @Bean
+    WebClient webClient(ReactiveClientRegistrationRepository clientRegistrations) {
+        InMemoryReactiveOAuth2AuthorizedClientService clientService = new InMemoryReactiveOAuth2AuthorizedClientService(clientRegistrations);
+        AuthorizedClientServiceReactiveOAuth2AuthorizedClientManager authorizedClientManager = new AuthorizedClientServiceReactiveOAuth2AuthorizedClientManager(clientRegistrations, clientService);
+        ServerOAuth2AuthorizedClientExchangeFilterFunction oauth = new ServerOAuth2AuthorizedClientExchangeFilterFunction(authorizedClientManager);
+        oauth.setDefaultClientRegistrationId("client2");
+        return WebClient.builder()
+                .filter(oauth)
+                .build();
+
+    }
+}
